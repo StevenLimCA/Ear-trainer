ec020aa0e44ca0406d340b4846fe4d26ce0ca5b3
Steven Lim <steven-lim-ca>
2022-08-25 16:09:07 -0700

    created responsive piano based on number of octaves and screen size

--- src/components/Piano/Piano.jsx
+++ src/components/Piano/Piano.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Octave from "../Octave/Octave";
 import "./Piano.scss";
 
 export default function Piano() {
-  const octaveArr = [0, 1, 2, 3, 4, 5, 6];
+  const [windowDimenion, detectHW] = useState({
+    winWidth: window.innerWidth,
+    winHeight: window.innerHeight,
+  });
+
+  const detectSize = () => {
+    detectHW({
+      winWidth: window.innerWidth,
+      winHeight: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    console.log(windowDimenion.winWidth);
+    if (windowDimenion.winWidth < 768) {
+      setOctaves([3]);
+    } else if (windowDimenion.winWidth < 1280) {
+      setOctaves([3, 4, 5]);
+    } else {
+      setOctaves([1, 2, 3, 4, 5, 6]);
+    }
+    window.addEventListener("resize", detectSize);
+
+    return () => {
+      window.removeEventListener("resize", detectSize);
+    };
+  }, [windowDimenion]);
+
+  const [octaves, setOctaves] = useState([0, 1, 2, 3, 4, 5, 6]);
 
   return (
     <div className="piano">
-      {octaveArr.map((octave) => (
+      {octaves.map((octave) => (
         <Octave octaveNum={octave} />
       ))}
     </div>
